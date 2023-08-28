bcedf71636022a62884658327c27960f2fa5b11a
Muaaz <muaazkhurshid@gmail.com>
2023-08-28 20:16:59 +0500

    Committed the logging.ini, logs printed for Aug 28,2023

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging.config
+
+
+def get_logger_obj() -> object:
+    """
+    Sets configs for Log obj and returns it.
+    """
+    logging.config.fileConfig('configs/logging.ini', disable_existing_loggers=False)
+    logger = logging.getLogger(__name__)
+
+    return logger
+
+logger = get_logger_obj()
+
+
+
+if __name__ == '__main__':
+    logger.info("Hello to Logging")
